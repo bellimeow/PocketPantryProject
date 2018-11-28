d880fa5c9b667c0da5bb8c4fe7a56b66a6511bdc
Isabella Delgado <isade842@student.liu.se>
2018-11-28 17:31:57 +0100

    Uploading working repository. Testing one feature at the time

---
+++ app/src/main/java/com/example/bella/pocketpantryproject/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.bella.pocketpantryproject;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Typeface iconFont = FontManager.getTypeface(getApplicationContext(), FontManager.FONTAWESOME);
+        FontManager.markAsIconContainer(findViewById(R.id.icons_container), iconFont);
+    }
+}
